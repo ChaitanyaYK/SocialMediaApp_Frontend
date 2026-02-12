ae13e101ab54a70d47fe32de5f4bca7599a420bd
ChaitanyaYK <chaitanyakolhe354@gmail.com>
2026-02-12 16:51:44 +0530

    Fix case-sensitive playlist import

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,7 @@ import videoSlice from './slices/videoSlice.js';
 import commentSlice from './slices/commentSlice.js';
 import subscriptionSlice from './slices/subscriptionSlice.js';
 import likeSlice from './slices/likeSlice.js';
-import playlistSlice from './slices/playListSlice.js'
+import playlistSlice from './slices/playlistSlice.js'
 
 const store = configureStore({
     reducer: {
